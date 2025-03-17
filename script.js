206395507519c2b93eece280f3f25ef2ea0828c0
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-17 18:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function mapLetters(str){
+	let obj={};
+	for (let i=0; i<str.length; i++) {
+		let char=str[i];
+		if (obj[char]===undefined) {
+			obj[char]=[];
+		}
+		obj[char].push(i);
+		
+	}
+	return obj;
+}
+let input= toLowerCase(prompt());
+alert(mapLetters(input));
